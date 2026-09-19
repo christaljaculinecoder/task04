d52933caad22e7552240513f3f6fbb6f9a6cad14
christaljaculinecoder <christaljaculine@gmail.com>
2026-09-20 01:37:49 +0530

    task04 commit

---
+++ script.js
@@ -0,0 +1,113 @@
+// calculator logic
+// let num1=15;
+// let num2=0;
+
+// let operator="/";
+
+// if (operator === "+") {
+// console.log(num1 + num2);
+// } 
+
+// else if (operator === "-") {
+//     console.log(num1 - num2);
+// }
+    
+// else if (operator === "*") {
+//     console.log(num1 * num2);
+// }
+// else if (operator === "/"){
+//     if(num2 === 0){
+// console.log("num2 cannot be Zero");
+//     }
+//     else{
+//         console.log(num1/num2);
+//     }
+//    if (num2!==0) { 
+//     console.log(num1/num2);
+//    }
+//    else {
+//     console.log("num2 cannot be Zero");
+// }
+// }
+
+
+//calculator logic with switch operator
+// let num1= 15;
+// let num2= 0;
+
+//  let operator="/"
+ 
+//  switch (operator){
+//     case "+" :
+//        console.log(num1 + num2);
+
+//     case "-" :
+//         console.log(num1 - num2);
+
+//     case "*" :
+//         console.log(num1 * num2) ;
+        
+//         case "/":
+//             if (num2===0){
+//                 console.log("num2 cannot be zero");
+//                 }
+//                 else {
+//                     console.log(num1 / num2);
+
+    
+//  }}
+
+// Student Results
+// let marks=39;
+// let bonus=10;
+
+// marks += bonus;
+// let Grade;
+
+// console.log("Marks:",marks);
+// if(marks >=90){
+//     console.log(Grade="Grade:A");    
+// }
+// else if (marks>= 70 ) {
+//     console.log( Grade="Grade:B")
+// }
+// else if (marks>=50) {
+//     console.log(Grade= "Grade:C")
+// }
+// else if (marks < 50) {
+//     console.log(Grade="Grade:F")
+
+// }
+
+// let Result = marks>=50 ? "Pass":"Fail";
+// console.log("Result:", Result);
+
+// switch(Grade){
+//     case "Grade:A":
+//     console.log("Remarks:Excellent!")
+//     break;
+
+//     case "Grade:B":
+//     console.log("Remarks:Good!")
+//     break;
+
+//     case "Grade:C":
+//     console.log("Remarks:Average")
+//     break;
+
+//     case "Grade:F":
+//     console.log("Remarks:Needs Improvement")
+//     break;
+// }
+// if (marks >= 90 && bonus > 0) {
+//     console.log("Extra Message: High marks with bonus!");}
+
+//     else if  (marks >= 70 && bonus > 0) {
+//     console.log("Extra Message: Good marks with bonus!");}
+
+//     else if (marks >= 50 && bonus > 0) {
+//     console.log("Extra Message: Average marks with bonus!");}
+    
+//     else if (marks <50 && bonus > 0) {
+//     console.log("Extra Message: Fail with Bonus!");
+// }
